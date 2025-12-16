97da10130175644496de13accadeee5b35b40857
Akshat Saini <akshatsaini497@gmail.com>
2025-12-16 16:28:56 +0530

    useeffect and sideeffects

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
+import {  useState } from 'react'
 import './App.css'
 import Header from './Header'
 function App() {
   const arr=["akshat",1,22.2,"hello"]
   // const obj={name:"akshat",age:25,phone:123456}
-  
+  const [show, setshow] = useState(true)
+ 
   return (
     <>
-    <Header/>
+    <button onClick={()=>{setshow(!show)}}>{show?"hide":"show"}</button>
+    {show&&<Header/>}
     {arr.map((value,index)=>{
       return(
        <div key={index} > {value}</div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,21 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Header(){
     const [first, setfirst] = useState(0)
 
 
+ useEffect(()=>{
 
+   const id= setInterval(() => {
+        console.log("hii")
+    }, 1000);
+
+    return ()=>{
+        clearInterval(id)
+    }
+   
+  },
+  [])
    function add(){
     setfirst(first+1)
     console.log(first)
